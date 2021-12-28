25dbb4135f938099a31b8a654650fb78544d1358
SodaCris <18463922396@163.com>
2021-12-28 10:27:20 +0800

    2to3 and add fix

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # py_socket_demo
 python sockets demo
+
+FROM: [stackoverflow](https://stackoverflow.com/q/51486786/12291425)
--- client_demo.py
+++ client_demo.py
@@ -1,49 +1,50 @@
 import socket
 import struct
 import threading
-import Queue
+import queue
 import time
 
 class ThreadedClient(threading.Thread):
     def __init__(self, host, port):
         threading.Thread.__init__(self)
         #set up queues
-        self.send_q = Queue.Queue(maxsize = 10)
+        self.send_q = queue.Queue(maxsize = 10)
         #declare instance variables       
         self.host = host
         self.port = port
         #connect to socket
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((self.host, self.port))
+        self.s.settimeout(.1)
 
     #LISTEN
     def listen(self):
         while True: #loop forever
             try:
-                print 'checking for message...'
+                print('checking for message...')
                 # stops listening if there's a message to send
                 if self.send_q.empty() == False:
                     self.send_message()
                 else:
-                    print 'no message'
-                print 'listening...'
-                message = self.s.recv(4096)
-                print 'RECEIVED: ' + message
+                    print('no message')
+                print('listening...')
+                message = self.s.recv(4096).decode()
+                print('RECEIVED: ' + message)
             except socket.timeout:
                 pass
 
     def start_listen(self):
-        t = threading.Thread(target = self.listen())
+        t = threading.Thread(target = self.listen)
         t.start()
         #t.join()
-        print 'started listen'
+        print('started listen')
 
 
     #ADD MESSAGE
     def add_message(self, msg):
         #put message into the send queue
         self.send_q.put(msg)
-        print 'ADDED MSG: ' + msg
+        print('ADDED MSG: ' + msg)
         #self.send_message()
 
     #SEND MESSAGE
@@ -51,10 +52,10 @@ class ThreadedClient(threading.Thread):
         #send message
         msg = self.get_send_q()
         if msg == "empty!":
-            print "nothing to send"
+            print("nothing to send")
         else:
-            self.s.sendall(msg)
-            print 'SENDING: ' + msg
+            self.s.sendall(msg.encode())
+            print('SENDING: ' + msg)
             #restart the listening
             #self.start_listen()
 
@@ -63,7 +64,7 @@ class ThreadedClient(threading.Thread):
     #if nothing in q, prints "empty" instead of stalling program
     def get_send_q(self):       
         if self.send_q.empty():
-            print "empty!"
+            print("empty!")
             return "empty!"
         else:
             msg = self.send_q.get()
@@ -74,7 +75,7 @@ if __name__ == '__main__':
     address = 'localhost'
     s = ThreadedClient(address, port)
     s.start()
-    print('Server started, port: ', port)
+    print(('Server started, port: ', port))
 
     s.add_message('hello world')
     s.start_listen()
